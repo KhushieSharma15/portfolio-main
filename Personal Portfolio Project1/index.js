2034270a26f7dcd11c6f7e320fd760f47fec27c6
khushisharmacanada123 <101332072+khushisharmacanada123@users.noreply.github.com>
2022-03-25 23:43:09 +0530

    Update index.js

--- Personal Portfolio Project1/index.js
+++ Personal Portfolio Project1/index.js
@@ -14,5 +14,3 @@ close.addEventListener("click", () => {
   document.querySelector(".modal").classList.remove("modal-active");
   document.querySelector(".overlay").classList.remove("overlay-active");
 });
-
-
